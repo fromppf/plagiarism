dc1223230f6ae0a737ae830259fb5fb809259f96
7IN0SAN9 <me@7in0.me>
2017-08-01 03:23:53 +0800

    support for Word97 files

--- parser.py
+++ parser.py
@@ -11,6 +11,7 @@ from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from pyth.plugins.rtf15.reader import Rtf15Reader
 from pyth.plugins.plaintext.writer import PlaintextWriter
 import html2text
+import olefile
 
 import jieba
 import jieba.analyse
@@ -39,7 +40,65 @@ def readfile(file):
         if file.endswith('.caj'):
             raise Exception('.caj files are not supported')
         elif file.endswith('.doc'):
-            raise Exception('.doc files are not supported')
+            text = ''
+            document = olefile.OleFileIO(data)
+
+            wordDocument = document.openstream('WordDocument').read()
+
+            # Parsing the WordDocument Stream
+            # See https://msdn.microsoft.com/en-us/library/office/dd904907(v=office.14).aspx
+            # And http://b2xtranslator.sourceforge.net/howtos/How_to_retrieve_text_from_a_binary_doc_file.pdf
+
+            # Loading the FIB
+            fib = wordDocument[:1472]
+
+            # Loading and Parsing the piece table
+            fcClx = int.from_bytes(fib[0x01A2:0x01A5], byteorder='little')
+            lcbClx = int.from_bytes(fib[0x01A6:0x01A9], byteorder='little')
+
+            tableFlag = ((int.from_bytes(fib[0x000A:0x000E], byteorder='little') & 0x0200) == 0x0200)
+            tableName = ('0Table', '1Table')[tableFlag]
+
+            table = document.openstream(tableName).read()
+
+            clx = table[fcClx:fcClx+lcbClx]
+
+            pos = 0
+            pieceTable = ''
+            lcbPieceTable = 0
+            while True:
+                if clx[pos] == 2:
+                    # this entry is the piece table
+                    lcbPieceTable = int.from_bytes(clx[pos+1:pos+5], byteorder='little')
+                    pieceTable = clx[pos+5:pos+5+lcbPieceTable]
+                    break
+                elif clx[pos] == 1:
+                    # skip this entry
+                    pos = pos + 1 + 1 + ord(clx[pos + 1])
+                else:
+                    break
+
+            i = 1
+            pieceCount = (lcbPieceTable - 4) / 12
+            while i <= pieceCount:
+                cpStart = int.from_bytes(pieceTable[i*4:i*4+4], byteorder='little')
+                cpEnd = int.from_bytes(pieceTable[(i+1)*4:(i+1)*4+4], byteorder='little')
+
+                offsetPieceDescriptor = int(((pieceCount + 1) * 4) + (i * 8))
+                pieceDescriptor = pieceTable[offsetPieceDescriptor:offsetPieceDescriptor+8]
+
+                fcValue = int.from_bytes(pieceDescriptor[2:6], byteorder='little')
+                isANSII = (fcValue & 0x40000000) == 0x40000000
+                fc = fcValue & 0xBFFFFFFF
+
+                encoding = ('utf-16', 'cp1252')[isANSII]
+                cb = cpEnd - cpStart
+                cb = (cb * 2, cb)[isANSII]
+                text += wordDocument[fc:fc+cb].decode(encoding)
+
+                i += 1
+
+            return text
         elif file.endswith('.docx'):
             text = ''
             document = Document(data)
